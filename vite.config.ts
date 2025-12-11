08525303160460d52e650ed493f1d44ffd239811
Marat <m.muhametshin@rambler.ru>
2025-12-11 03:37:11 +0300

    fix: set correct base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/Doska.app/',
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+    },
+  },
 })
